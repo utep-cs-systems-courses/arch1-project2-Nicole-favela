fdb69bf3a4c32ec546fcdf2ab80731d2516bb80a
Nicole Favela <nefavela@miners.utep.edu>
2020-03-20 00:04:30 -0600

    added function calls to main

---
+++ project/lab_2/my_blink_demo/blinkMain.c
@@ -0,0 +1,18 @@
+//Alternate LEDs from Off, Green, and Red
+#include <msp430.h>
+#include "timerLib/libTimer.h"
+#include "led.h"
+#include "my_buzzer.h"
+#include "my_switches.h"
+
+
+int main(void) {
+  configureClocks();		/* setup master oscillator, CPU & peripheral clocks */
+  led_init();
+  switch_init();
+  buzzer_init();
+  
+  enableWDTInterrupts();	/* enable periodic interrupt */
+
+  or_sr(0x18);		/* CPU off, GIE on */
+}
