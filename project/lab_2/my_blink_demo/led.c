11741f7776628b3ba879c4aa2be04dfeb509ae6c
Nicole Favela <nefavela@miners.utep.edu>
2020-03-21 23:14:22 -0600

    added dimming

--- project/lab_2/my_blink_demo/led.c
+++ project/lab_2/my_blink_demo/led.c
@@ -2,12 +2,25 @@
 #include "led.h"
 #include "my_switches.h"
 #include "my_buzzer.h"
-
+#include "stateMachines.h"
 //unsigned char red_on = 0, green_on = 0;/
 //unsigned char led_changed = 0;
-
+char button = 0;
 static char redVal[] = {0, LED_RED}, greenVal[] = {0, LED_GREEN};
-
+void turn_led_off()
+{
+  char ledFlags = 0;
+  P1OUT &= (0xff-LEDS) | ledFlags;
+  P1OUT |= ledFlags;
+}
+void turn_led_on()
+{
+  char ledFlags = 0;
+  ledFlags |= LED_GREEN;
+  ledFlags |= LED_RED;
+  P1OUT &= (0xff -LEDS) | ledFlags;
+  P1OUT |=ledFlags;
+}
 
 void led_init()
 {
@@ -16,17 +29,34 @@ void led_init()
   led_update();
 }
 
-void led_update()
+void led_update(button)
 {
-  /*if (led_changed) {
+  if (led_changed) {
     char ledFlags = redVal[red_on] | greenVal[green_on];
     
-    P1OUT &= (0xff^LEDS) | ledFlags; // clear bit for off leds
+    P1OUT &= (0xff-LEDS) | ledFlags; // clear bit for off leds
     P1OUT |= ledFlags;		     // set bit for on leds
-    led_changed = 0;
   }
-  */
-  if(switch_state_down){
+    if(switch_state_changed){
+      char ledFlags = 0;
+      switch(select_button(button)){
+      case 1:
+	ledFlags |= LED_RED;
+	break;
+      case 2:
+	ledFlags |= LED_GREEN;
+	break;
+      case 3:
+	ledFlags = 0;
+	break;
+      case 4:
+	ledFlags = 0;
+	break;
+    }
+	P1OUT &= (0xff-LEDS) | ledFlags;
+	P1OUT |= ledFlags;
+    }
+      /* if(switch_state_down){
     char ledFlags = 0;
     ledFlags |= switch_state_down ? LED_GREEN :0;
     ledFlags |= switch_state_down ? 0:LED_RED;
@@ -34,7 +64,7 @@ void led_update()
     P1OUT |= ledFlags;
   
   }
-  else if(sw_state_down_1){
+  if(sw_state_down_1){
     char ledFlags = 0;
     ledFlags |= sw_state_down_1 ? LED_RED : 0;
     ledFlags |= sw_state_down_1 ? 0: LED_GREEN;
@@ -42,22 +72,32 @@ void led_update()
     P1OUT |= ledFlags;
 
   }
-  else if (sw_state_down_2){
-    buzzer_set_period(1000);
-  }
-  else if(sw_state_down_3){
+  if (sw_state_down_2){//button 3
     char ledFlags = 0;
-    ledFlags |= sw_state_down_3 ? 0:0;
+    // ledFlags |= sw_state_down_2;
+    // ledFlags |= sw_state_down_2 ? LED_GREEN: LED_RED;
+    P1OUT &= (0xff ^LEDS) | ledFlags;
+    P1OUT |=ledFlags;
+    
+  }
+  if(sw_state_down_3){
+    char ledFlags = 0;
+    /* ledFlags |= sw_state_down_3 ? 0:0;
 
     P1OUT &= (0xff -LEDS) | ledFlags;
     P1OUT |= ledFlags;
-    
+      *//*
+    turn_led_off();
   }
-  else if(led_changed){
-    char ledFlags = redVal[red_on] | greenVal[green_on];
-    P1OUT &=(0xff ^ LEDS) | ledFlags;
+  if(led_changed){
+     char ledFlags = redVal[red_on] | greenVal[green_on];
+    P1OUT &=(0xff - LEDS) | ledFlags;
     P1OUT |= ledFlags;
+   
+    // turn_led_on();
   }
+  
   switch_state_changed = 0;
+	*/
 }
 
