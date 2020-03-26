48dd42b1ad815771b9fe415ffbe1b13d923baad6
Nicole Favela <nefavela@miners.utep.edu>
2020-03-25 23:38:13 -0600

    added comments

--- project/lab_2/my_blink_demo/my_switches.h
+++ project/lab_2/my_blink_demo/my_switches.h
@@ -6,11 +6,14 @@
 #define SW2 BIT2
 #define SW3 BIT3
 /* switch1 is p1.3 */
+//SW0 = button 1 (S1 on msp430), SW1 = botton 2, SW3 = button 3,SW4= button 4
 #define SWITCHES (SW0 | SW1 | SW2 | SW3)		/* only 1 switch on this board */
 
 void switch_init();
 void switch_interrupt_handler();
 
+//where I define my buttons
+//switch_state_down is first button S1
 extern char switch_state_down, switch_state_changed , sw_state_down_1, sw_state_down_2,sw_state_down_3; /* effectively boolean */
 
 #endif // included
